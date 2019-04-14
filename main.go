934c398d8a87ddbc5e29dbf2e5c1b184608f0813
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2019-04-14 20:58:51 +0200

    Allow API handlers to use the Database

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/aojea/TODO/internal/handlers"
+	"github.com/aojea/todo/internal/handlers"
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gorilla/mux"
@@ -13,22 +13,22 @@ import (
 
 func main() {
 	// Connect to the Database
-	DB, err := sql.Open("mysql",
+	db, err := sql.Open("mysql",
 		"root:password@tcp(127.0.0.1:3306)/todo_db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer DB.Close()
+	defer db.Close()
 	// Initialize the API server
 	r := mux.NewRouter()
 	// API version 1
 	api := r.PathPrefix("/api/v1").Subrouter()
 	// TODO lists handlers
-	api.HandleFunc("/user/me/lists", handlers.ListsHandler)
-	api.HandleFunc("/user/me/lists/{listId}", handlers.ListIDHandler)
+	api.HandleFunc("/user/me/lists", handlers.ListsHandler(db))
+	api.HandleFunc("/user/me/lists/{listId}", handlers.ListIDHandler(db))
 	// Tasks handlers
-	api.HandleFunc("/lists/{listId}/tasks", handlers.TasksHandler)
-	api.HandleFunc("/lists/{listId}/tasks/{taskId}", handlers.TaskIDHandler)
+	api.HandleFunc("/lists/{listId}/tasks", handlers.TasksHandler(db))
+	api.HandleFunc("/lists/{listId}/tasks/{taskId}", handlers.TaskIDHandler(db))
 	// Run the app
 	http.ListenAndServe(":8080", r)
 }
