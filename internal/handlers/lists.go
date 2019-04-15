b46839df36089691c7f557ad77db0564c0a55f38
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2019-04-15 15:39:45 +0200

    Add lists API update functionality

--- internal/handlers/lists.go
+++ internal/handlers/lists.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -14,14 +13,18 @@ import (
 type list struct {
 	ID       int    `json:"listId"`
 	Title    string `json:"title"`
-	Username string `json:"username"`
+	Username string `json:"username,omitempty"`
 }
 
 func (l *list) updateList(db *sql.DB) error {
-	return errors.New("Not implemented")
+	// Note: It doesn't fail if id is not valid
+	statement := fmt.Sprintf("UPDATE lists SET title='%s' WHERE id=%d", l.Title, l.ID)
+	_, err := db.Exec(statement)
+	return err
 }
 
 func (l *list) deleteList(db *sql.DB) error {
+	// Note: It doesn't fail if id is not valid
 	statement := fmt.Sprintf("DELETE FROM lists WHERE id=%d", l.ID)
 	_, err := db.Exec(statement)
 	return err
@@ -57,7 +60,7 @@ func getLists(db *sql.DB) ([]list, error) {
 	return lists, nil
 }
 
-// ListsHandler operates over the TODO lists
+// ListsHandler operates over the TODO lists /user/me/lists
 func ListsHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
@@ -91,7 +94,7 @@ func ListsHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// ListIDHandler operater on a specific list
+// ListIDHandler operater on a specific list /user/me/list/{listId}
 func ListIDHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Obtain the list id from the request
@@ -106,8 +109,20 @@ func ListIDHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		// PUT: Update list id
 		case http.MethodPut:
-			w.WriteHeader(http.StatusOK)
-		// DELETE: Delete list id
+			decoder := json.NewDecoder(r.Body)
+			if err := decoder.Decode(&l); err != nil {
+				respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+				return
+			}
+			defer r.Body.Close()
+			// TODO: check if id payload is different to the URL one and fail
+			// we hardcode it to the one provided in the API URL
+			l.ID = id
+			if err := l.updateList(db); err != nil {
+				respondWithError(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+			respondWithJSON(w, http.StatusOK, l)
 		case http.MethodDelete:
 			if err := l.deleteList(db); err != nil {
 				respondWithError(w, http.StatusInternalServerError, err.Error())
